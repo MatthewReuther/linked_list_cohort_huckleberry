8df460791f1d84a9ca8cb35ce710e406f0d4d21d
Matthew Reuther <matthew.reuther@gmail.com>
2015-04-13 15:45:07 -0500

    completed 2nd task by adding attr_reader :next_item

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,3 +1,16 @@
 class LinkedListItem
   include Comparable
+
+
+  attr_reader :payload
+  def initialize(payload)
+    @payload = payload
+  end
+
+  attr_reader :next_item
+  def next_item=(following_item)
+    raise ArgumentError if following_item === self
+    @next_item = following_item
+  end
+
 end
